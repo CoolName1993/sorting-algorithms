c45e44da0845c2286cc2d2a0662940ff0ec5d8cf
christopher-boucher <christopher.boucher@digital.hmrc.gov.uk>
2017-05-15 14:33:54 +0100

    Added quicksort implementation using filters

--- src/main/scala/sorting/QuickSort.scala
+++ src/main/scala/sorting/QuickSort.scala
@@ -1,5 +1,18 @@
 package sorting
 
-class QuickSort {
+object QuickSort {
+
+  def sort(array: Seq[Int]): Seq[Int] = {
+    def quicksort(a: Seq[Int]): Seq[Int] = {
+      if (a.length < 2) a // If list cannot be divided by 2 and return an integer
+      else {
+        val pivot = a(a.length / 2) // Select the middle element of the list
+        quicksort(a.filter(pivot >)) ++ // Perform quicksort on all elements with a lesser value than the pivot value
+        a.filter(pivot ==) ++ // All elements that are equal need not be quicksorted
+        quicksort(a.filter(pivot <)) // Perform quicksort on all elements with a greater value than the pivot value
+      }
+    }
+    quicksort(array)
+  }
 
 }
