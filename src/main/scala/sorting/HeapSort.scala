d273c35b8279a3cdd8fcb89132a4a88302cb7e49
christopher-boucher <christopher.boucher@digital.hmrc.gov.uk>
2017-05-15 21:30:10 +0100

    Added heap sort class and tests

---
+++ src/main/scala/sorting/HeapSort.scala
@@ -0,0 +1,11 @@
+package sorting
+
+/**
+  * Worst case performance: O(n log n) Memory: 1
+  * Best case performance: O(n log n) Memory: 1
+  */
+object HeapSort extends Sorting {
+
+  override def sort(array: Seq[Int]): Seq[Int] = ???
+
+}
---
+++ src/test/scala/sorting/HeapSortSpec.scala
@@ -0,0 +1,29 @@
+package sorting
+
+import testutils.UnitTest
+
+class HeapSortSpec extends UnitTest {
+
+  "sort" must {
+
+    "sort a sequence of integers" in {
+      val sequence = Seq(10,9,8,7,6,5,4,3,2,1,0)
+      val result = HeapSort.sort(sequence)
+      result mustBe Seq(0,1,2,3,4,5,6,7,8,9,10)
+    }
+
+    "sort another sequence of integers" in {
+      val sequence = Seq(10,5,9,4,8,1,7,0,6,1,5,5,4,2,3,7,2,1,1,0)
+      val result = HeapSort.sort(sequence)
+      result mustBe  Seq(0,0,1,1,1,1,2,2,3,4,4,5,5,5,6,7,7,8,9,10)
+    }
+
+    "sort a large sequence of integers" in {
+      val sequence = 2000000 to 0 by -1
+      val result = HeapSort.sort(sequence)
+      result mustBe  (0 to 2000000)
+    }
+
+  }
+
+}
